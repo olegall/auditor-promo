d03527c08a6ea64b5f87d1b33ee9abb35373b33f
Oleg Aleynikov <aleynikov@bit-it.ru>
2019-11-29 11:48:31 +0300

    Доработал скрипты

--- sendMail.php
+++ sendMail.php
@@ -1,5 +1,5 @@
 <?php
-if(!$_POST) exit;
+/*if(!$_POST) exit;
 
 $name     = $_POST['name'];
 $phone    = $_POST['phone'];
@@ -28,4 +28,31 @@ if(mail($address, $e_subject, $msg, $headers)) {
 	echo "</fieldset>";
 } else {
 	echo 'ERROR!';
-}
+}*/
+
+
+if(!$_POST) exit;
+
+$name     = $_POST['name'];
+$phone    = $_POST['phone'];
+$email    = $_POST['email'];
+$comment  = $_POST['comment'];
+
+$address = "marketing@bit-it.ru, aleynikov.oleg@gmail.com";
+
+$subject = 'Запрос с сайта БИТ Аудитор';
+$e_subject = '=?utf-8?B?'.base64_encode($subject).'?=';
+
+$msg = "С формы сообщения на лэндинге пишет $name" . PHP_EOL .
+	   'Тел.: '.$phone."\n".
+	   "Email: $email"."\n".
+	   "Комментарий: $comment";
+
+$headers = "From: no-reply@bit-it.ru" . PHP_EOL;
+$headers .= "Reply-To: $email" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+echo mail($address, $e_subject, $msg, $headers); 
+?>
